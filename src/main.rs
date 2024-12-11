89098b4216265a0e74f53e439675da1f4e8a9285
Isaac <isaacloff@gmail.com>
2024-12-11 21:19:28 +0000

    Batman

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+enum Player {
+    Noughts,
+    Crosses,
+}
+
+struct State {
+
+}
+
+const M: usize = 3;
+const N: usize = 3;
+
+fn main() {
+    let mut weighted_states: Vec<[[(State, f64); N]; M]> = vec![];
+}
